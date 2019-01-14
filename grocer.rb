99701284062c5cc4b796da4345c0ebe55fb22d22
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-14 05:02:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,44 @@
 def consolidate_cart(cart)
   # code here
+  temp = {}
+  
+   cart.each do |item|
+    item.each do |food, att|
+      if !temp.has_key?(food)
+        temp[food] = { :price => att[:price], :clearance => att[:clearance], :count => 1 }
+      else
+        temp[food][:count] += 1
+      end
+    end
+  end
+  
+  return temp
 end
+
+def process_coupon(cart, coupon)
+
+    numofcoupons = coupon[:num]
+    itemForCoupon = coupon[:item]
+    numberofcoupons = coupon[:num].to_i
+  
+    if cart.has_key?(itemForCoupon) && cart[itemForCoupon][:count] >= numberofcoupons
+      cart[itemForCoupon][:count] = cart[itemForCoupon][:count] - numofcoupons
+      cart["#{itemForCoupon} W/COUPON"] ||= {}
+      cart["#{itemForCoupon} W/COUPON"][:clearance] = cart[itemForCoupon][:clearance]
+      cart["#{itemForCoupon} W/COUPON"][:price] = coupon[:cost]
+      cart["#{itemForCoupon} W/COUPON"][:count] ||= 0
+      cart["#{itemForCoupon} W/COUPON"][:count] += 1
+    end
+  end
+  
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |couponitem|
+    process_coupon(cart, couponitem)
+  end
+    #cart
+    return cart
 end
 
 def apply_clearance(cart)
